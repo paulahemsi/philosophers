9540f29bfa1ad9d5d6f6bfefe8d02847abdb7dcb
paulahemsi <paulahemsi@gmail.com>
2021-08-15 19:17:43 -0300

    colorfull output

--- philo/includes/colors.h
+++ philo/includes/colors.h
@@ -6,29 +6,103 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 19:50:41 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/08/11 19:53:34 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/08/15 19:15:56 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_H
 # define COLORS_H
 
-# define BLACK "\033[0;30m"
-# define BOLD_BLACK "\033[1;30m"
-# define RED "\033[0;31m"
-# define BOLD_RED "\033[1;31m"
-# define GREEN "\033[0;32m"
-# define BOLD_GREEN "\033[1;32m"
-# define YELLOW "\033[0;33m"
-# define BOLD_YELLOW "\033[1;33m"
-# define BLUE "\033[0;34m"
-# define BOLD_BLUE "\033[1;34m"
-# define PURPLE "\033[0;35m"
-# define BOLD_PURPLE "\033[1;35m"
-# define CYAN "\033[0;36m"
-# define BOLD_CYAN "\033[1;36m"
-# define WHITE "\033[0;37m"
-# define BOLD_WHITE "\033[1;37m"
-# define RESET_COLOR "\033[0m"
+/*
+** TEXT
+*/
+
+/*
+** regular
+*/
+#define RED "\e[0;31m"
+#define GREEN "\e[0;32m"
+#define BLUE "\e[0;34m"
+#define CYAN "\e[0;36m"
+#define MAGENTA "\e[0;35m"
+#define YELLOW "\e[0;33m"
+#define BLACK "\e[0;30m"
+#define WHITE "\e[0;37m"
+/*
+** bold
+*/
+#define BOLD_RED "\e[1;31m"
+#define BOLD_GREEN "\e[1;32m"
+#define BOLD_BLUE "\e[1;34m"
+#define BOLD_CYAN "\e[1;36m"
+#define BOLD_MAGENTA "\e[1;35m"
+#define BOLD_YELLOW "\e[1;33m"
+#define BOLD_BLACK "\e[1;30m"
+#define BOLD_WHITE "\e[1;37m"
+/*
+** underline
+*/
+#define UNDER_RED "\e[4;31m"
+#define UNDER_GREEN "\e[4;32m"
+#define UNDER_BLUE "\e[4;34m"
+#define UNDER_CYAN "\e[4;36m"
+#define UNDER_MAGENTA "\e[4;35m"
+#define UNDER_YELLOW "\e[4;33m"
+#define UNDER_BLACK "\e[4;30m"
+#define UNDER_WHITE "\e[4;37m"
+/*
+** High intensty
+*/
+#define H_RED "\e[0;91m"
+#define H_GREEN "\e[0;92m"
+#define H_BLUE "\e[0;94m"
+#define H_CYAN "\e[0;96m"
+#define H_MAGENTA "\e[0;95m"
+#define H_YELLOW "\e[0;93m"
+#define H_BLACK "\e[0;90m"
+#define H_WHITE "\e[0;97m"
+/*
+** Bold high intensity
+*/
+#define BOLD_H_BLACK "\e[1;90m"
+#define BOLD_H_RED "\e[1;91m"
+#define BOLD_H_GREEN "\e[1;92m"
+#define BOLD_H_YELLOW "\e[1;93m"
+#define BOLD_H_BLUE "\e[1;94m"
+#define BOLD_H_MAGENTA "\e[1;95m"
+#define BOLD_H_CYAN "\e[1;96m"
+#define BOLD_H_WHITE "\e[1;97m"
+
+/*
+** BACKGROUND
+*/
+
+/*
+** Regular
+*/
+#define RED_BACK "\e[41m"
+#define GREEN_BACK "\e[42m"
+#define BLUE_BACK "\e[44m"
+#define CYAN_BACK "\e[46m"
+#define MAGENTA_BACK "\e[45m"
+#define YELLOW_BACK "\e[43m"
+#define BLACK_BACK "\e[40m"
+#define WHITE_BACK "\e[47m"
+/*
+** High intensty
+*/
+#define H_RED_BACK "\e[0;101m"
+#define H_GREEN_BACK "\e[0;102m"
+#define H_BLUE_BACK "\e[0;104m"
+#define H_CYAN_BACK "\e[0;106m"
+#define H_MAGENTA_BACK "\e[0;105m"
+#define H_YELLOW_BACK "\e[0;103m"
+#define H_BLACK_BACK "\e[0;100m"
+#define H_WHITE_BACK "\e[0;107m"
+
+/*
+** RESET
+*/
+#define RESET_COLOR "\e[0m"
 
 #endif
--- philo/sources/simulation/actions.c
+++ philo/sources/simulation/actions.c
@@ -6,49 +6,43 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 09:43:48 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/08/15 18:08:03 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/08/15 19:14:43 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	print_status(long long int time, int philo_number, pthread_mutex_t *text, char *status)
+static void	print_status(long long int time, int philo_number, pthread_mutex_t *text, char *status, char *color)
 {
 	pthread_mutex_lock(text);
-	printf("%lld %d %s\n", time, philo_number, status);
+	printf("%s%-10lld %-3d %-20s%s\n", color, time, philo_number, status, RESET_COLOR);
 	pthread_mutex_unlock(text);
 }
 
 void	sleeping(t_philo *philo)
 {
-	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, SLEEPING);
+	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, SLEEPING, H_BLUE);
 	do_action(philo->time->to_sleep);
 }
 
 void	eat(t_philo *philo)
 {
-	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, EATING);
+	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, EATING, H_MAGENTA);
 	do_action(philo->time->to_eat);
 	pthread_mutex_unlock(&philo->forks[philo->hand[LEFT]]);
 	pthread_mutex_unlock(&philo->forks[philo->hand[RIGHT]]);
-	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, "STOP EATING");
+	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, "STOP EATING", H_MAGENTA);
 }
 
 void	think(t_philo *philo)
 {
-	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, THINKING);
+	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, THINKING, H_CYAN);
 }
 
 void	take_forks(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->forks[philo->hand[LEFT]]);
-	pthread_mutex_lock(&philo->mutex->text);
-	printf("fork adress: %p\n", &philo->forks[philo->hand[LEFT]]);
-	pthread_mutex_unlock(&philo->mutex->text);
-	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, FORK);
+	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, FORK, H_YELLOW);
 	pthread_mutex_lock(&philo->forks[philo->hand[RIGHT]]);
-	pthread_mutex_lock(&philo->mutex->text);
-	printf("fork adress: %p\n", &philo->forks[philo->hand[RIGHT]]);
-	pthread_mutex_unlock(&philo->mutex->text);
-	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, FORK);
+	print_status(get_elapsed_time(philo->time->start), philo->index, &philo->mutex->text, FORK, H_YELLOW);
 }
