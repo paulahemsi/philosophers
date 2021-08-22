2867ec3d8e4644ff22b96504c5055cd6aaa95964
paulahemsi <paulahemsi@gmail.com>
2021-08-21 23:27:44 -0300

    is_death if last_meal=0 out

--- philo/sources/simulation/check_end.c
+++ philo/sources/simulation/check_end.c
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 18:34:07 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/08/21 22:38:25 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/08/21 23:26:44 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,6 @@ bool	is_death(t_philo *philo)
 
 	if (anyone_dead(philo))
 		return (true);
-	if (philo->last_meal == 0)
-		return (false);
 	now = get_elapsed_time(philo->dinner->time.start);
 	if ((now - philo->last_meal) > philo->dinner->time.to_die)
 	{
